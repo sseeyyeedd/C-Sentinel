29254fef3dc4fd31789ed85c50851f16cff9d82f
unknown <mazloomi2002@gmail.com>
2025-09-08 17:28:43 +0330

    change main based on checker and transformer

--- main.py
+++ main.py
@@ -2,56 +2,47 @@ from antlr4 import *
 import argparse
 from parser import parse_dsl_rules
 from cparser import parse_c_code
-from engine import SecurityAuditor
+from checker import run_checker
+from transformer import normalize_ast
 
 def main():
-    # بخش ۱: دریافت ورودی‌ها از خط فرمان
     arg_parser = argparse.ArgumentParser(description='CSentinel - A C Static Analyzer based on a custom DSL.')
     arg_parser.add_argument('--rules', required=True, help='Path to the CSentinel rule file (.sen)')
     arg_parser.add_argument('--source', required=True, help='Path to the C source file to analyze')
     args = arg_parser.parse_args()
 
-    # بخش ۲: فراخوانی پارسرها
     print(f"[*] Loading rules from: {args.rules}")
     dsl_rules = parse_dsl_rules(args.rules)
     print(f"[*] Successfully loaded {len(dsl_rules)} rule(s).")
-    # print(dsl_rules) # می‌توانید برای دیباگ کردن، قوانین خوانده شده را چاپ کنید
 
     print(f"[*] Parsing C source from: {args.source}")
-    try :
-        with open(args.source,'r',encoding='utf-8')as f:
+    try:
+        with open(args.source,'r',encoding='utf-8') as f:
             source_code_lines = f.readlines()
     except Exception as e:
         print(f"[!] Error reading source file: {e}")
         return
+
     c_ast = parse_c_code(args.source)
     if not c_ast:
         print("[!] Failed to parse C code. Please check the source file for errors. Exiting.")
         return
     print("[*] C code parsed successfully into an AST.")
-    # print(c_ast)
 
+    print("[*] Normalizing AST...")
+    c_ast, mods = normalize_ast(c_ast)
+    if mods:
+        print(f"[*] AST normalized with {len(mods)} modification(s).")
 
-    # بخش ۳: فراخوانی موتور تحلیل (در گام بعدی تکمیل می‌شود)
     print("\n--- Running Security Analysis ---")
+    errors = run_checker(dsl_rules, c_ast, source_code_lines)
 
-    # auditor = SecurityAuditor(dsl_rules)
-    # errors = auditor.run_analysis(c_ast)
-
-    # فعلا به صورت موقت یک لیست خالی برای خطاها در نظر می‌گیریم
-    auditor = SecurityAuditor(dsl_rules, source_code_lines)
-    errors = auditor.run_analysis(c_ast)
-
-    # بخش ۴: گزارش نتایج
     if errors:
-        print(f"\n[!] Found {len(errors)} security violation(s):")
-        print()
+        print(f"\n[!] Found {len(errors)} security violation(s):\n")
         for error in errors:
-            print(f"  - {error}")
-            print()
+            print(f"  - {error}\n")
     else:
-        print("\n[*] Analysis complete. No violations found (engine not yet implemented).")
-
+        print("\n[*] Analysis complete. No violations found.")
 
 if __name__ == '__main__':
     main()
